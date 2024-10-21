d5af732cad2012b7bf52d122f006af23e83786e0
monikhoder <rinmorni@gmail.com>
2024-10-21 22:48:16 +0700

    Add project files.

---
+++ FTPBookDownload/MainForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace FTPBookDownload
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            KimTools.WinForms.KtTableColumn ktTableColumn1 = new KimTools.WinForms.KtTableColumn();
+            this.FrmTheme = new KimTools.WinForms.KtTheme(this.components);
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.tblBook = new KimTools.WinForms.KtTable();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDownloadLoading = new System.Windows.Forms.Label();
+            this.btnDownload = new KimTools.WinForms.KtButton();
+            this.cover_panel = new System.Windows.Forms.Panel();
+            this.lblLoadingCover = new System.Windows.Forms.Label();
+            this.BookCover = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.TexbookLoading = new System.Windows.Forms.Label();
+            this.bookTextBox = new System.Windows.Forms.TextBox();
+            this.LeftPanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.cover_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookCover)).BeginInit();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FrmTheme
+            // 
+            this.FrmTheme.Accent = System.Drawing.Color.Empty;
+            this.FrmTheme.Contrast = 10D;
+            this.FrmTheme.Dark = false;
+            this.FrmTheme.Primary = System.Drawing.Color.Empty;
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.Controls.Add(this.tblBook);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(187, 450);
+            this.LeftPanel.TabIndex = 0;
+            // 
+            // tblBook
+            // 
+            this.tblBook.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(222)))), ((int)(((byte)(225)))));
+            ktTableColumn1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.NotSet;
+            ktTableColumn1.Fill = "";
+            ktTableColumn1.Format = "";
+            ktTableColumn1.Group = "";
+            ktTableColumn1.IsPrimaryKey = false;
+            ktTableColumn1.Key = "tblname";
+            ktTableColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.NotSet;
+            ktTableColumn1.Search = true;
+            ktTableColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            ktTableColumn1.Template = "";
+            ktTableColumn1.Title = "Book Title";
+            ktTableColumn1.ToolTip = "";
+            ktTableColumn1.Type = KimTools.WinForms.KtTableColumnTypes.Text;
+            ktTableColumn1.Value = "";
+            ktTableColumn1.Visible = true;
+            ktTableColumn1.Width = 0;
+            ktTableColumn1.Width_ = "";
+            this.tblBook.Columns = new KimTools.WinForms.KtTableColumn[] {
+        ktTableColumn1};
+            this.tblBook.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblBook.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tblBook.KeyCaseSensitive = false;
+            this.tblBook.Latency = 500;
+            this.tblBook.Location = new System.Drawing.Point(0, 0);
+            this.tblBook.MultiSelect = false;
+            this.tblBook.Name = "tblBook";
+            this.tblBook.Padding = new System.Windows.Forms.Padding(5);
+            this.tblBook.ShowFooter = false;
+            this.tblBook.ShowHeader = true;
+            this.tblBook.ShowSearch = true;
+            this.tblBook.Size = new System.Drawing.Size(187, 450);
+            this.tblBook.TabIndex = 0;
+            this.tblBook.CellClick += new KimTools.WinForms.KtTable.KtTableRowCellEventArgs(this.tblBook_CellClick);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.TopPanel.Controls.Add(this.panel1);
+            this.TopPanel.Controls.Add(this.cover_panel);
+            this.TopPanel.Controls.Add(this.lblTitle);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(187, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(613, 133);
+            this.TopPanel.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblDownloadLoading);
+            this.panel1.Controls.Add(this.btnDownload);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(423, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(190, 133);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblDownloadLoading
+            // 
+            this.lblDownloadLoading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDownloadLoading.AutoSize = true;
+            this.lblDownloadLoading.Location = new System.Drawing.Point(29, 77);
+            this.lblDownloadLoading.Name = "lblDownloadLoading";
+            this.lblDownloadLoading.Size = new System.Drawing.Size(136, 25);
+            this.lblDownloadLoading.TabIndex = 4;
+            this.lblDownloadLoading.Text = "Downloading...";
+            this.lblDownloadLoading.Visible = false;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.BackColor = System.Drawing.Color.Empty;
+            this.btnDownload.BorderShape = KimTools.WinForms.KtSize.Default;
+            this.btnDownload.BorderSize = KimTools.WinForms.KtSize.Default;
+            this.btnDownload.CustomColors = new System.Drawing.Color[0];
+            this.btnDownload.FlatAppearance.BorderSize = 0;
+            this.btnDownload.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnDownload.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownload.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDownload.IconName = "Tabler.outline.download";
+            this.btnDownload.IconSize = 16;
+            this.btnDownload.IconStroke = 2.5D;
+            this.btnDownload.Location = new System.Drawing.Point(34, 23);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(126, 44);
+            this.btnDownload.Style = KimTools.WinForms.KtStyle.Tailwind;
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // cover_panel
+            // 
+            this.cover_panel.Controls.Add(this.lblLoadingCover);
+            this.cover_panel.Controls.Add(this.BookCover);
+            this.cover_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.cover_panel.Location = new System.Drawing.Point(0, 0);
+            this.cover_panel.Name = "cover_panel";
+            this.cover_panel.Size = new System.Drawing.Size(123, 133);
+            this.cover_panel.TabIndex = 3;
+            // 
+            // lblLoadingCover
+            // 
+            this.lblLoadingCover.AutoSize = true;
+            this.lblLoadingCover.BackColor = System.Drawing.Color.Transparent;
+            this.lblLoadingCover.Location = new System.Drawing.Point(30, 42);
+            this.lblLoadingCover.Name = "lblLoadingCover";
+            this.lblLoadingCover.Size = new System.Drawing.Size(91, 25);
+            this.lblLoadingCover.TabIndex = 1;
+            this.lblLoadingCover.Text = "Loading...";
+            // 
+            // BookCover
+            // 
+            this.BookCover.Location = new System.Drawing.Point(21, 9);
+            this.BookCover.Name = "BookCover";
+            this.BookCover.Size = new System.Drawing.Size(86, 93);
+            this.BookCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BookCover.TabIndex = 0;
+            this.BookCover.TabStop = false;
+            this.BookCover.WaitOnLoad = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(154, 46);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(129, 32);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Book Title";
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.White;
+            this.MainPanel.Controls.Add(this.TexbookLoading);
+            this.MainPanel.Controls.Add(this.bookTextBox);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(187, 133);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(613, 317);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // TexbookLoading
+            // 
+            this.TexbookLoading.AutoSize = true;
+            this.TexbookLoading.BackColor = System.Drawing.Color.Transparent;
+            this.TexbookLoading.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TexbookLoading.Location = new System.Drawing.Point(0, 0);
+            this.TexbookLoading.Name = "TexbookLoading";
+            this.TexbookLoading.Size = new System.Drawing.Size(91, 25);
+            this.TexbookLoading.TabIndex = 2;
+            this.TexbookLoading.Text = "Loading...";
+            // 
+            // bookTextBox
+            // 
+            this.bookTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bookTextBox.Location = new System.Drawing.Point(0, 0);
+            this.bookTextBox.Multiline = true;
+            this.bookTextBox.Name = "bookTextBox";
+            this.bookTextBox.ReadOnly = true;
+            this.bookTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.bookTextBox.Size = new System.Drawing.Size(613, 317);
+            this.bookTextBox.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.LeftPanel);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(40)))), ((int)(((byte)(67)))));
+            this.Name = "MainForm";
+            this.Opacity = 1D;
+            this.Text = "FTP BOOK DOWNLOAD";
+            this.Theme = this.FrmTheme;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.LeftPanel.ResumeLayout(false);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.cover_panel.ResumeLayout(false);
+            this.cover_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookCover)).EndInit();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private KimTools.WinForms.KtTheme FrmTheme;
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Panel MainPanel;
+        private KimTools.WinForms.KtTable tblBook;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox BookCover;
+        private KimTools.WinForms.KtButton btnDownload;
+        private System.Windows.Forms.Panel cover_panel;
+        private System.Windows.Forms.Label lblLoadingCover;
+        private System.Windows.Forms.Label TexbookLoading;
+        private System.Windows.Forms.TextBox bookTextBox;
+        private System.Windows.Forms.Label lblDownloadLoading;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ FTPBookDownload/MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KimTools.WinForms;
+
+namespace FTPBookDownload
+{
+    public partial class MainForm : KtWindow
+    {
+        private List<Book> bookList = new List<Book>();
+        private void AddBookList()
+        {
+            bookList.Add(new Book { Id = "01", Title = "Pride and Prejudice", url = "ftp://gutenberg.pglaf.org/1/3/4/1342/1342-0.txt", img_url = "https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "02", Title = "The Adventures of Sherlock Holmes", url = "ftp://gutenberg.pglaf.org/1/6/6/1661/1661-0.txt", img_url = "https://www.gutenberg.org/cache/epub/1661/pg1661.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "03", Title = "Moby Dick", url = "ftp://gutenberg.pglaf.org/2/7/0/2701/2701-0.txt", img_url = "https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "04", Title = "War and Peace", url = "ftp://gutenberg.pglaf.org/2/6/0/2600/2600-0.txt", img_url = "https://www.gutenberg.org/cache/epub/2600/pg2600.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "05", Title = "The Great Gatsby", url = "ftp://gutenberg.pglaf.org/6/4/3/64317/64317-0.txt", img_url = "https://www.gutenberg.org/cache/epub/64317/pg64317.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "06", Title = "Frankenstein", url = "ftp://gutenberg.pglaf.org/8/4/84/84-0.txt", img_url = "https://www.gutenberg.org/cache/epub/84/pg84.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "07", Title = "Dracula", url = "ftp://gutenberg.pglaf.org/3/4/345/345-0.txt", img_url = "https://www.gutenberg.org/cache/epub/345/pg345.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "08", Title = "The Odyssey", url = "ftp://gutenberg.pglaf.org/1/7/2/1727/1727-0.txt", img_url = "https://www.gutenberg.org/cache/epub/1727/pg1727.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "09", Title = "Ulysses", url = "ftp://gutenberg.pglaf.org/4/3/0/4300/4300-0.txt", img_url = "https://www.gutenberg.org/cache/epub/4300/pg4300.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "10", Title = "A Tale of Two Cities", url = "ftp://gutenberg.pglaf.org/9/8/98/98-0.txt", img_url = "https://www.gutenberg.org/cache/epub/98/pg98.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "11", Title = "Jane Eyre", url = "ftp://gutenberg.pglaf.org/1/2/6/1260/1260-0.txt", img_url = "https://www.gutenberg.org/cache/epub/1260/pg1260.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "12", Title = "Les Misérables", url = "ftp://gutenberg.pglaf.org/1/3/5/135/135-0.txt", img_url = "https://www.gutenberg.org/cache/epub/135/pg135.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "13", Title = "The Picture of Dorian Gray", url = "ftp://gutenberg.pglaf.org/1/7/4/174/174-0.txt", img_url = "https://www.gutenberg.org/cache/epub/174/pg174.cover.medium.jpg" });
+            bookList.Add(new Book { Id = "14", Title = "The Count of Monte Cristo", url = "ftp://gutenberg.pglaf.org/1/1/8/1184/1184-0.txt", img_url = "https://www.gutenberg.org/cache/epub/1184/pg1184.cover.medium.jpg" });
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void LoadBooks()
+        {
+            tblBook.Rows.Clear();
+            foreach (var book in bookList)
+            {
+                var books = tblBook.NewRow();
+                books["tblname"] = book.Title;
+            }
+
+
+        }
+        public class Book
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public string url { get; set; }
+            public string img_url { get; set; }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            AddBookList();
+            LoadBooks();
+            
+        }
+
+        private async void tblBook_CellClick(KtTable table, KtTableRow row, KtTableColumn column)
+        {
+            int rowindex = row.Index;
+            string imagUrl = bookList[rowindex].img_url;
+            lblTitle.Text = bookList[rowindex].Title;
+            ChangeBookCover(imagUrl);
+            bookTextBox.Clear();
+            ReadBookAsync(rowindex);
+
+        }
+        private async Task ChangeBookCover(string url)
+        {
+            try
+            {
+                lblLoadingCover.Visible = true;
+
+                // Download the image asynchronously
+                using (WebClient webClient = new WebClient())
+                {
+                    byte[] imageBytes = await webClient.DownloadDataTaskAsync(new Uri(url));
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        // Set the downloaded image to the picture box
+                        BookCover.Image = Image.FromStream(ms);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions (e.g., network issues)
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                lblLoadingCover.Visible = false;
+            }
+        }
+        private async Task ReadBookAsync(int index)
+        {
+            string url = bookList[index].url;
+            string result = string.Empty;
+
+            TexbookLoading.Visible = true;
+
+            try
+            {
+                // Create FTP web request
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                // Get FTP response
+                using (FtpWebResponse response = (FtpWebResponse)await Task.Factory.FromAsync(
+                    request.BeginGetResponse, request.EndGetResponse, null))
+                using (Stream responseStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    // Read data from file
+                    result = await reader.ReadToEndAsync();
+
+                    // Save to a local file
+                    File.WriteAllText("book.txt", result);
+
+                    // Set the text to the bookTextBox
+                    bookTextBox.Text = result;
+                }
+            }
+            catch (WebException webEx)
+            {
+                // Handle web exceptions (e.g., network issues, file not found)
+                MessageBox.Show($"Error accessing the FTP server: {webEx.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (IOException ioEx)
+            {
+                // Handle IO exceptions (e.g., file access issues)
+                MessageBox.Show($"Error reading or writing the file: {ioEx.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                TexbookLoading.Visible = false;
+            }
+
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            int rowIndex = 1;
+            string url = bookList[rowIndex].url;
+            string localFilePath = SelectSaveFilePath($"{lblTitle.Text}.txt");
+            if (localFilePath != null)
+            {
+                lblDownloadLoading.Visible = true;
+                await DownloadFileFromFTP(url, localFilePath);
+                lblDownloadLoading.Visible = false;
+            }
+
+        }
+        private string SelectSaveFilePath(string defaultFileName)
+        {
+          
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.FileName = defaultFileName;
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.Title = "Save the FTP File";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return saveFileDialog.FileName;
+                }
+            }
+            return null;
+        }
+        private async Task DownloadFileFromFTP(string ftpUrl, string localFilePath)
+        {
+            try
+            {
+                // Create an FtpWebRequest for the given FTP URL
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = new NetworkCredential("anonymous", "anonymous");
+
+                // Get the FTP response
+                using (FtpWebResponse response = (FtpWebResponse)await Task.Factory.FromAsync(
+                    request.BeginGetResponse, request.EndGetResponse, null))
+                using (Stream responseStream = response.GetResponseStream())
+                using (FileStream fileStream = new FileStream(localFilePath, FileMode.Create))
+                {
+                    // Copy the FTP file to the local file system
+                    await responseStream.CopyToAsync(fileStream);
+                }
+
+                MessageBox.Show("Download completed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show($"Error during FTP download: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"File system error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
